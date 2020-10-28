1c407610771bec1b400e685664f086587e6e99ed
Maureen <chiamaureenla@gmail.com>
2020-10-28 13:28:35 +0800

    關於--微調

---
+++ homepage/src/components/Product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Product.sass'
+
+function Product() {
+  return (
+    <>
+      <div className="product">
+        <div className="title">
+          <img src="/images/title-product.png" alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Product
