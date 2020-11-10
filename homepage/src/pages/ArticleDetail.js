15ee3b7c57db96aaaa06ddaf18887a20a3f6867e
Maureen <chiamaureenla@gmail.com>
2020-11-10 10:23:38 +0800

    文章的部分

--- homepage/src/pages/ArticleDetail.js
+++ homepage/src/pages/ArticleDetail.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../App.css'
 //import Navbar from '../conponents/common/Navbar'
-import Type from '../components/conponents/Type'
-import Main from '../components/conponents/Main'
+import Type from '../components/article/Type'
+import Main from '../components/article/Main'
 // import Featured from '../conponents/forMain/Featured'
 //import Footer from '../conponents/common/Footer'
 
